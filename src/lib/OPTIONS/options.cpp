a4c4b9c6a356d1f7f35af8b361b2a6360b6cc924
Blane Townsend <blane.townsend@gmail.com>
2026-05-18 09:49:12 -0500

    Moved baud rates back

--- src/lib/OPTIONS/options.cpp
+++ src/lib/OPTIONS/options.cpp
@@ -201,7 +201,7 @@ static void options_LoadFromFlashOrFile(EspFlashStream &strmFlash)
     firmwareOptions.uart_baud = doc["rcvr-uart-baud"] | USE_AIRPORT_AT_BAUD;
     firmwareOptions.is_airport = doc["is-airport"] | true;
     #else
-    firmwareOptions.uart_baud = doc["rcvr-uart-baud"] | 115200;
+    firmwareOptions.uart_baud = doc["rcvr-uart-baud"] | 420000;
     firmwareOptions.is_airport = doc["is-airport"] | false;
     #endif
     firmwareOptions.lock_on_first_connection = doc["lock-on-first-connection"] | true;
